e9be537d436de90ef4b6a7c4b24708e8c7245545
Brian Fincher <brian@fincherhome.com>
2014-09-12 18:58:29 -0500

    fix delete orphan releases

--- misc/testing/delete_orphaned_musicinfo.php
+++ misc/testing/delete_orphaned_musicinfo.php
@@ -1,17 +1,18 @@
 <?php
 define('FS_ROOT', realpath(dirname(__FILE__)));
 require_once(FS_ROOT."/../../www/config.php");
-require_once(FS_ROOT."/../../www/lib/framework/db.php");
 
-$db = new Db;
-$sql = "select ID from musicinfo where ID not in (select musicinfoID from releases where musicinfoID is not null)";
-$rel = $db->query($sql);
-echo "Deleting ".count($rel)." orphaned musicinfo entries\n";
+$pdo = new nzedb\db\Settings();
+$sql = "select mi.ID as ID from releases r right join musicinfo mi on r.musicinfoID = mi.ID where r.musicinfoID is null;";
+$rel = $pdo->query($sql);
+#echo "Deleting ".count($rel)." orphaned musicinfo entries\n";
 foreach ($rel as $r) { 
-	$file = "../../www/covers/music/".$r['ID'].".jpg";
-	#unlink($file);
+#	echo var_dump($r);
+        echo $r['id']."\n";
+	$file = "../../www/covers/music/".$r['id'].".jpg";
+	unlink($file);
 
-	$db->query(sprintf("delete from musicinfo where ID = %d", $r['ID']));
+	$pdo->queryExec(sprintf("delete from musicinfo where ID = %d", $r['id']));
 }
 
 ?>
